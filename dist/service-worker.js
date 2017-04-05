7db3adad0403aa91792cbf0495a08aa566a63bdb
Anisha <ab6544@nyu.edu>
2017-04-05 18:55:24 -0400

    Modified appcache

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -8,7 +8,7 @@ var urlsToCache = [
   '//yoav-zibin.github.io/TicTacToe/dist/js/everything.min.js',
   '//yoav-zibin.github.io/TicTacToe/dist/css/everything.min.css',
 ];
-var CACHE_NAME = 'cache-v2017-04-05T22:52:11.962Z';
+var CACHE_NAME = 'cache-v2017-04-05T22:55:15.159Z';
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(
