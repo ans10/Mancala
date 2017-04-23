051f8e0194eb0a820097105a09271e1f8221b5f4
Anisha <ab6544@nyu.edu>
2017-04-23 18:31:55 -0400

    Minified

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -8,7 +8,7 @@ var urlsToCache = [
   '//yoav-zibin.github.io/TicTacToe/dist/js/everything.min.js',
   '//yoav-zibin.github.io/TicTacToe/dist/css/everything.min.css',
 ];
-var CACHE_NAME = 'cache-v2017-04-21T16:44:44.422Z';
+var CACHE_NAME = 'cache-v2017-04-23T22:20:26.549Z';
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(
