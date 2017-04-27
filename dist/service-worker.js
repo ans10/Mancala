6c3abe16b03e444906733d80e8b0f008cc6470b5
ans10 <ans556@nyu.edu>
2017-04-27 02:42:47 -0400

    trying bigger bg

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -8,7 +8,7 @@ var urlsToCache = [
   '//yoav-zibin.github.io/TicTacToe/dist/js/everything.min.js',
   '//yoav-zibin.github.io/TicTacToe/dist/css/everything.min.css',
 ];
-var CACHE_NAME = 'cache-v2017-04-27T06:33:32.003Z';
+var CACHE_NAME = 'cache-v2017-04-27T06:42:03.002Z';
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(
