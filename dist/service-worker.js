025873f698964cc85f3d3eb18f97ea7b50a9d178
ans10 <ans556@nyu.edu>
2017-04-28 01:37:13 -0400

    Trying to improve bg on android devices

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -8,7 +8,7 @@ var urlsToCache = [
   '//yoav-zibin.github.io/TicTacToe/dist/js/everything.min.js',
   '//yoav-zibin.github.io/TicTacToe/dist/css/everything.min.css',
 ];
-var CACHE_NAME = 'cache-v2017-04-27T06:42:03.002Z';
+var CACHE_NAME = 'cache-v2017-04-28T05:36:37.946Z';
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(
