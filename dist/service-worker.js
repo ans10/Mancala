3fc05fd525900a7456e1d83c27433e74049e99bc
Anisha <ab6544@nyu.edu>
2017-05-07 09:47:55 -0400

    Adding track by

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -8,7 +8,7 @@ var urlsToCache = [
   '//yoav-zibin.github.io/TicTacToe/dist/js/everything.min.js',
   '//yoav-zibin.github.io/TicTacToe/dist/css/everything.min.css',
 ];
-var CACHE_NAME = 'cache-v2017-05-06T21:11:42.630Z';
+var CACHE_NAME = 'cache-v2017-05-07T13:45:56.411Z';
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(
